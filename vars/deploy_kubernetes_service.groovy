73600063f21601c07bb6b45e55ab1bdcf3983314
Makara1122 <mam.makara.1221@rupp.edu.kh>
2024-11-20 15:59:47 +0700

    hello

--- vars/deploy_kubernetes_service.groovy
+++ vars/deploy_kubernetes_service.groovy
@@ -1,12 +1,19 @@
 def call(String appName) {
-    // Debug: List current directories
-    sh "echo 'Listing files in current directory'; pwd; ls -al"
-
-    // Debug: List files in ansible workspace
-    sh "echo 'Listing files in ansible workspace'; ls -al /home/jenkins/ansible_workspace"
-
-    // Run the Ansible playbook
+    // Debug: List current directory
     sh """
-        ansible-playbook -i /home/jenkins/ansible_workspace/inventory.ini /home/jenkins/ansible_workspace/deploy-playbook-with-sh-v2.yml -e "app_name=${appName}"
+        echo 'Listing files in current directory';
+        pwd;
+        ls -al;
+    """
+    
+    // Debug: List files in the correct workspace path
+    sh """
+        echo 'Listing files in the Jenkins workspace';
+        ls -al /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace;
+    """
+
+    // Run the Ansible playbook with corrected paths
+    sh """
+        ansible-playbook -i /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/inventory.ini /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/deploy-playbook-with-sh-v2.yml -e "app_name=${appName}"
     """
 }
