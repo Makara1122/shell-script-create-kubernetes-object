5fa020bc4adf47bc5d45acab3a19eb7ac1761746
Makara1122 <mam.makara.1221@rupp.edu.kh>
2024-11-20 15:58:02 +0700

    hello

--- vars/deploy_kubernetes_service.groovy
+++ vars/deploy_kubernetes_service.groovy
@@ -1,8 +1,12 @@
 def call(String appName) {
-    // Ensure that Ansible is installed on the Jenkins agent
+    // Debug: List current directories
+    sh "echo 'Listing files in current directory'; pwd; ls -al"
+
+    // Debug: List files in ansible workspace
+    sh "echo 'Listing files in ansible workspace'; ls -al /home/jenkins/ansible_workspace"
+
+    // Run the Ansible playbook
     sh """
-ansible-playbook -i inventory.ini deploy-playbook-with-sh-v2.yml -e "app_name=${appName}"
-
-"""
-
+        ansible-playbook -i /home/jenkins/ansible_workspace/inventory.ini /home/jenkins/ansible_workspace/deploy-playbook-with-sh-v2.yml -e "app_name=${appName}"
+    """
 }
