578014cbf7da9fb3b0b03006d9d739409eeea054
Makara1122 <mam.makara.1221@rupp.edu.kh>
2024-11-20 15:41:17 +0700

    hello

--- vars/deploy_kubernetes_service.groovy
+++ vars/deploy_kubernetes_service.groovy
@@ -1,6 +1,8 @@
 def call(String appName) {
     // Ensure that Ansible is installed on the Jenkins agent
     sh """
-        ansible-playbook deploy-playbook-with-sh-v2.yml -e "app_name=${appName}"
-    """
+ansible-playbook -i inventory.ini deploy-playbook-with-sh-v2.yml -e "app_name=${appName}"
+
+"""
+
 }
