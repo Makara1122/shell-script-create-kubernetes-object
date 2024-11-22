4513f99df44f54d5a314069991fea07378bbf511
Makara <148768781+Makara1122@users.noreply.github.com>
2024-11-22 17:37:27 +0700

    Update deploy_kubernetes_service.groovy

--- vars/deploy_kubernetes_service.groovy
+++ vars/deploy_kubernetes_service.groovy
@@ -1,4 +1,25 @@
-def call(String appName) {
+// def call(String appName) {
+//     // Debug: List current directory
+//     sh """
+//         echo 'Listing files in current directory';
+//         pwd;
+//         ls -al;
+//     """
+    
+//     // Debug: List files in the correct workspace path
+//     sh """
+//         echo 'Listing files in the Jenkins workspace';
+//         ls -al /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace;
+//     """
+
+//     // Run the Ansible playbook with corrected paths
+//     sh """
+//         ansible-playbook -i /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/inventory.ini /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/vars/deploy-playbook-with-sh-v2.yml -e "app_name=${appName}"
+//     """
+// }
+
+
+def call(String appName, String imageName) {
     // Debug: List current directory
     sh """
         echo 'Listing files in current directory';
@@ -14,6 +35,6 @@ def call(String appName) {
 
     // Run the Ansible playbook with corrected paths
     sh """
-        ansible-playbook -i /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/inventory.ini /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/vars/deploy-playbook-with-sh-v2.yml -e "app_name=${appName}"
+        ansible-playbook -i /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/inventory.ini /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/vars/deploy-playbook-with-sh-v2.yml -e "app_name=${appName}" -e "image_name=${imageName}"
     """
 }
