c0ed1580283a3d3a1203070210de0d60de7d81e5
Makara <148768781+Makara1122@users.noreply.github.com>
2024-11-25 13:08:17 +0700

    Update deploy_kubernetes_service.groovy

--- vars/deploy_kubernetes_service.groovy
+++ vars/deploy_kubernetes_service.groovy
@@ -51,11 +51,11 @@ def call(String appName, String imageName) {
     // Debug: List files in the correct workspace path
     sh """
         echo 'Listing files in the Jenkins workspace';
-        ls -al /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace;
+        ls -al /var/lib/jenkins/workspace/makara-infrastructure/workspace;
     """
 
     // Run the Ansible playbook with corrected paths
     sh """
-        ansible-playbook -i /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/inventory.ini /home/jenkins/.jenkins/jobs/jenkins-ansible-v1/workspace/vars/deploy-playbook-with-sh-v2.yml -e "app_name=${appName}" -e "image_name=${imageName}"
+        ansible-playbook -i /var/lib/jenkins/workspace/makara-infrastructure/workspace/inventory.ini /var/lib/jenkins/workspace/makara-infrastructure/workspace/vars/deploy-playbook-with-sh-v2.yml -e "app_name=${appName}" -e "image_name=${imageName}"
     """
 }
